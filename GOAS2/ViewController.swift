eaab3b8af494c089f8795e1b5ea281b442ad3780
Cathy Chen <cathychen@cathys-macbook-air.local>
2019-01-15 20:44:17 -0800

    Initial Commit

---
+++ GOAS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOAS2
+//
+//  Created by Cathy Chen on 1/15/19.
+//  Copyright © 2019 Fortune Cookies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
