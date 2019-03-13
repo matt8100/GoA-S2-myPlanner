9dc94442cbe939f1a0dd6ed0a6f179a0d125bee8
Cathy Chen <cathychen@cathys-air.rsd38.ca>
2019-03-12 21:28:04 -0700

    Added image

--- GOAS2/ViewController.swift
+++ GOAS2/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+<<<<<<< Updated upstream
         print ("Hello GOAS2")
+=======
+        
+>>>>>>> Stashed changes
     }
 
 
