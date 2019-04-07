f2da54641908bbff53e064c05206b34c1af1fabb
Matthew Mao <matthewmao@Matthews-iMac.local>
2019-04-06 20:46:41 -0700

    calendar test

---
+++ Homework App (GOAS2)/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  Homework App (GOAS2)
+//
+//  Created by Matthew Mao on 2019-04-02.
+//  Copyright © 2019 Fortune Cookies. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
