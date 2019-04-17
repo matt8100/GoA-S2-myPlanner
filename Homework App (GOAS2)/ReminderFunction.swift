800bb16d883f4a679cade9f770fef1e486b3097a
Student <mathhow26@rocketmail.com>
2019-04-16 21:06:57 -0700

    deleted reminder

--- Homework App (GOAS2)/ReminderFunction.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ReminderFunction.swift
-//  Homework App (GOAS2)
-//
-//  Created by Student on 2019-04-16.
-//  Copyright © 2019 Fortune Cookies. All rights reserved.
-//
-
-import Foundation
